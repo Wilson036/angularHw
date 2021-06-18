6014f069f497fea6e8adb4b325c117a27c09b02c
wilsonkao <wilkao036@gmail.com>
2021-06-19 02:03:16 +0800

    init setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Products {
+  id?: number;
+  manufactory?: string;
+  type?: string;
+  minPrice?: string;
+  price?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angularHw';
+  id: number | undefined = 0;
+  products: Products[] = [];
+  selectedProduct3: Products = {};
+
+  profileForm = new FormGroup({
+    manufactory: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required),
+    minPrice: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+  });
+
+  constructor() {}
+
+  ngOnInit() {
+    this.products = [
+      {
+        id: 1,
+        manufactory: 'bmw',
+        type: 'bmw',
+        minPrice: '9.9',
+        price: '9.2',
+      },
+    ];
+  }
+
+  insert() {
+    const data = this.profileForm.value;
+    const validate = Object.values(data)
+      .map(Boolean)
+      .every((val) => val);
+    if (!validate) {
+      return;
+    }
+    this.products.push({ id: this.products.length + 1, ...data });
+  }
+
+  clear() {
+    this.profileForm.reset();
+  }
+
+  update() {
+    if (this.id) {
+      this.products = this.products.map((product) => {
+        if (product.id === this.id) {
+          product = { ...product, ...this.profileForm.value };
+        }
+        return product;
+      });
+    }
+  }
+
+  setValues(product: Products) {
+    this.profileForm.patchValue({ ...product });
+    this.id = product.id;
+  }
+
+  delete(productId: number) {
+    this.products = this.products.filter(({ id }) => id !== productId);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    RippleModule,
+    TableModule,
+    ToastModule,
+    InputTextModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
